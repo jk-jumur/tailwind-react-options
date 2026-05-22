16564566734e16c9bdaaa8c86d80775da0619998
jk-jumur <developer.jumur@gmail.com>
2026-05-23 00:45:31 +0600

    pricing card to modify for the component

--- src/Components/PricingOptions/PricingOptions.jsx
+++ src/Components/PricingOptions/PricingOptions.jsx
@@ -1,5 +1,7 @@
 import { use } from "react";
-import PricingCard from "../PricingCard/PricingCard";
+import DaisyPricingCard from "../DaisyPricingCard/DaisyPricingCard";
+// import PricingCard from "../PricingCard/PricingCard";
+
 
 
 const PricingOptions = ({pricingPromise}) => {
@@ -9,8 +11,12 @@ const PricingOptions = ({pricingPromise}) => {
         <div>
              <h2 className="text-5xl">Get Our Membership</h2>
              <div className="grid  md:grid-cols-2  lg:grid-cols-3   gap-8 m-5">
-                 {
+                 {/* {
                       pricingData.map(pricing => <PricingCard key={pricing.price} pricing={pricing}></PricingCard>)
+                 } */}
+
+                 {
+                     pricingData.map(pricing => <DaisyPricingCard key={pricing.price} pricing={pricing}></DaisyPricingCard>)
                  }
              </div>
         </div>

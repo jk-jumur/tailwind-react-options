04b38376e5d751bd1364081a8144cba2201f53a0
jk-jumur <developer.jumur@gmail.com>
2026-05-23 00:04:56 +0600

    pricing card design

--- src/Components/PricingCard/PricingCard.jsx
+++ src/Components/PricingCard/PricingCard.jsx
@@ -4,7 +4,7 @@ import PricingFeature from "./PricingFeature";
 const PricingCard = ({pricing}) => {
        const{name, price, description, features} = pricing;
     return (
-        <div className="border bg-amber-400 rounded-3xl p-3 text-white">
+        <div className="flex flex-col border bg-amber-400 rounded-3xl p-3 text-white">
             {/* card header */}
             <div>
                   <h1 className="text-6xl text-white">{name}</h1>
@@ -12,12 +12,14 @@ const PricingCard = ({pricing}) => {
             </div>
 
             {/* card body */}
-             <div className="bg-amber-300 p-2 mt-10 rounded-2xl">
+             <div className="bg-amber-300 p-2 mt-10  flex-1 rounded-2xl  ">
                 <p>{description}</p>
                  {
                     features.map((feature, index) => <PricingFeature  key={index} feature={feature}></PricingFeature>) 
                  }
              </div>
+             <button className="btn w-full 
+              bg-pink-300 text-white  mt-3 md:mt-4 hover:bg-pink-500 rounded-2xl">Subscribe</button>
         </div>
     );
 };
--- src/Components/PricingCard/PricingFeature.jsx
+++ src/Components/PricingCard/PricingFeature.jsx
@@ -3,7 +3,8 @@ import { CircleCheckBig } from "lucide-react";
 
 const PricingFeature = ({feature}) => {
     return (
-           <p className="flex mt-4"><CircleCheckBig className="mr-4"></CircleCheckBig>{feature}</p>
+           <p className="flex mt-4"><CircleCheckBig className="mr-4" color="#5a36b0" size={32}></CircleCheckBig>{feature}</p>
+           
     );
 };
 
